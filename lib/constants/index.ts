51f4bc9f5d0cb5976f418c8d6bbdec6a27694f56
thi-miranda-97 <thimiranda97@gmail.com>
2025-01-13 18:33:03 -0700

    Initial commit

---
+++ lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Potzyn";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "Potzyn is a modern dynamic full-stack e-commerce platform built using Next.js";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
