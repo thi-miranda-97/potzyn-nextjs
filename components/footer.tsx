96b0346f4f1480134c27905ed3a88e6f4c8f004d
thi-miranda-97 <thimiranda97@gmail.com>
2025-01-19 11:07:18 -0700

    add back to top function

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,20 +5,53 @@ import NavLink from "./shared/header/navlink";
 import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import XIcon from "@mui/icons-material/X";
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+
 export default function Footer() {
+  const [isVisible, setIsVisible] = useState(false);
+  const currentYear = new Date().getFullYear();
+
+  // Handle visibility of "Back to Top" button based on scroll position
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsVisible(window.scrollY > 300); // Update visibility based on scroll position
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // Scroll to the top function
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <footer className="wrapper">
       <div className="flex-between border-t py-3 lg:py-6">
         <Logo />
         <NavLink />
-        <div className="flex justify-end gap-3 lg:gap-6 body-1">
+        <div className="flex items-center justify-end gap-3 lg:gap-6 body-1">
           <FacebookRoundedIcon />
           <InstagramIcon />
           <XIcon />
-          <span className="">Back to top &uarr;</span>
+          {isVisible && (
+            <Button
+              variant="outline"
+              className="cursor-pointer hover:scale-105 transition-transform"
+              onClick={scrollToTop}
+            >
+              Back to top &uarr;
+            </Button>
+          )}
         </div>
       </div>
-      {/* FOOTER IMAGE BACKGROUND */}
+
+      {/* Footer Image Background */}
       <div className="relative h-[200px] w-full flex items-center justify-center">
         <div
           className="absolute inset-0 bg-cover bg-center"
@@ -33,7 +66,7 @@ export default function Footer() {
       </div>
 
       <div className="text-center text-[0.5rem] lg:text-[1rem] text-gray-500">
-        &copy;2025 Potzyn. All Rights Reserved.
+        &copy;{currentYear} Potzyn. All Rights Reserved.
       </div>
     </footer>
   );
