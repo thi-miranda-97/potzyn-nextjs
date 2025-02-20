7190d19a1aaf9f35c78edf988edd11735558e4ec
thi-miranda-97 <thimiranda97@gmail.com>
2025-02-20 16:15:40 -0700

    add autoprefixer

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ const barlow = Barlow({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   display: "swap",
-  preload: true,
 });
 
 export const metadata: Metadata = {
