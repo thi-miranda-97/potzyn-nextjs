360308545cf691d7c258e570bb16a631c96a47fc
thi-miranda-97 <thimiranda97@gmail.com>
2025-02-20 13:01:36 -0700

    update signOutUser action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -15,6 +15,7 @@ import { PAGE_SIZE } from "../constants";
 import { revalidatePath } from "next/cache";
 import { updateUserSchema } from "../validators";
 import { Prisma } from "@prisma/client";
+import { getMyCart } from "./cart.actions";
 
 
 export const signInWithCredentials = async (
@@ -40,7 +41,10 @@ export const signInWithCredentials = async (
 
 // Sign user out
 export async function signOutUser() {
-  await signOut();
+    // get current users cart and delete it so it does not persist to next user
+    const currentCart = await getMyCart();
+    await prisma.cart.delete({ where: { id: currentCart?.id } });
+    await signOut();
 }
 
 //  Sign up user
